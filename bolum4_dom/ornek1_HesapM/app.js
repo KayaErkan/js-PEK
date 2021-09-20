536af3a4da9d3d62e502e53e2720d68466771a8e
ipek <ipekbilir79@gmail.com>
2021-09-20 22:13:05 +0300

    ewewe

---
+++ bolum4_dom/ornek1_HesapM/app.js
@@ -0,0 +1,6 @@
+function calcNumbers(result) {
+  form.displayResult.value = form.displayResult.value + result;
+}
+document.querySelector(".button light Cey").onClick = function () {
+  document.querySelector(".ekranBorder").value = "";
+};
